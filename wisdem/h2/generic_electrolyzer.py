3ca1c1fb52e30233bf6c999827479494d64ee77c
dakotaramos <ramos.dakotasky@gmail.com>
2022-06-30 08:53:52 -0600

    updated wisdem version of analytic electrolyzer to match h2atscale

--- wisdem/h2/generic_electrolyzer.py
+++ wisdem/h2/generic_electrolyzer.py
@@ -30,24 +30,25 @@ class electrolyzer:
         self.F = 96485.34 # Faraday's constant (C/mol)
         self.R = 8.314 # Ideal gas constant J/(mol*K)
         self.n = 2 # number of electrons transfered in reaction
+        self.E_th_0 = 1.481 # Thermoneutral voltage at standard state
         
 
 
         # Assumptions: TODO: update with corrected assumptions for H2@Scale project, possible allow as arguments specified during call?
         self.n_cells = 100 # Number of cells
-        self.cell_area = 1250/self.n_cells # Cell area (cm^2)
+        self.cell_area = 1580 # Cell active area (cm^2)
         self.stack_rating_kW = 750 # Stack rating (kW)
         self.stack_input_voltage = 250 # VDC 
         # self.h2_pres_out = 31 # H2 outlet pressure (bar)
 
-    def converter_efficiency(self, Pdc): # from empirical ESIF model, unsure if used elsewhere in WEIS/WISDEM
-        a = -7.798034801
-        b = -20.201370915
-        c = 8.704400555
-        d = -17.680617589
+    # def converter_efficiency(self, Pdc): # from empirical ESIF model, unsure if used elsewhere in WEIS/WISDEM
+    #     a = -7.798034801
+    #     b = -20.201370915
+    #     c = 8.704400555
+    #     d = -17.680617589
 
-        efficiency = (a * Pdc) / (b + Pdc) + (c * Pdc) / (d + Pdc)
-        return efficiency
+    #     efficiency = (a * Pdc) / (b + Pdc) + (c * Pdc) / (d + Pdc)
+    #     return efficiency
 
     def convert_ac_to_dc(self, Pac): # from empirical ESIF model, can we leave this or should we get a generalized converter model?
         """
@@ -60,23 +61,23 @@ class electrolyzer:
         Pdc_minus = (-b - np.sqrt(b ** 2 - 4 * a * (c - Pac))) / (2 * a)
         return Pdc_minus, Pdc_plus
 
-    def stack_current_from_voltage(self, V_in, T): # from empirical ESIF model, unsure if used elsewhere in WEIS/WISDEM
-        a1 = -0.000146297
-        b1 = 0.025443613
-        c1 = -0.178823095
-        d1 = 168.652142633
+    # def stack_current_from_voltage(self, V_in, T): # from empirical ESIF model, unsure if used elsewhere in WEIS/WISDEM
+    #     a1 = -0.000146297
+    #     b1 = 0.025443613
+    #     c1 = -0.178823095
+    #     d1 = 168.652142633
 
-        I = (V_in - c1 * T - d1) / (a1 * T + b1)
-        return I
+    #     I = (V_in - c1 * T - d1) / (a1 * T + b1)
+    #     return I
 
-    def stack_voltage_from_current(self, I_in, T): # from empirical ESIF model, unsure if used elsewhere in WEIS/WISDEM
-        a1 = -0.000146297
-        b1 = 0.025443613
-        c1 = -0.178823095
-        d1 = 168.652142633
+    # def stack_voltage_from_current(self, I_in, T): # from empirical ESIF model, unsure if used elsewhere in WEIS/WISDEM
+    #     a1 = -0.000146297
+    #     b1 = 0.025443613
+    #     c1 = -0.178823095
+    #     d1 = 168.652142633
 
-        V = a1 * I_in * T + b1 * I_in + c1 * T + d1
-        return V
+    #     V = a1 * I_in * T + b1 * I_in + c1 * T + d1
+    #     return V
 
     def stack_current_from_power(self, P_in, T): 
         ### Old empirical model can be removed
@@ -98,59 +99,83 @@ class electrolyzer:
         
         T_K = T + 273.15 
         # Cell reversible voltage:
-        E_rev_0 = 1.229  # Reversible cell voltage at standard state, TODO: update to be f(T)?
-        E_th_0 = 1.481 # Thermoneutral voltage at standard state (V), TODO: update to be f(T)?
-        p_atmo = 101325 # (Pa) atmospheric pressure / pressure of water TODO: validate this is accurate
-        p_anode = 101325 # (Pa) pressure at anode, assumed atmo TODO: validate this is accurate
-        p_cathode = 101325 #(Pa) pressure at cathode, assumed atmo TODO: validate this is accurate
+        E_rev_0 = 1.229  # Reversible cell voltage at standard state
+        p_atmo = 101325 # (Pa) atmospheric pressure / pressure of water 
+        p_anode = 101325 # (Pa) pressure at anode, assumed atmo 
+        p_cathode = 3e+6 #(Pa) pressure at cathode, assumed atmo 
         p_h2O_sat = 19946 # (Pa) @60C TODO: update to be f(T)? https://www.engineeringtoolbox.com/water-vapor-saturation-pressure-d_599.html?vA=60&units=C#
         E_rev = E_rev_0 + ((self.R*T_K)/(self.n*self.F)) * (np.log(((p_anode-p_h2O_sat)/p_atmo) * math.sqrt((p_cathode-p_h2O_sat)/p_atmo))) # General Nernst equation
 
         # Activation overpotential:
         # constants below assumed from https://www.sciencedirect.com/science/article/pii/S0360319916318341?via%3Dihub 
-        # TODO: update based on feedback
-        T_anode = T_K # TODO: updated with realistic anode temperature?
+        T_anode = T_K # TODO: updated with realistic anode temperature? 70-80 C nominal operating temperature 58C
         T_cathode = T_K # TODO: updated with realistic anode temperature?
         alpha_a = 2 # anode charge transfer coefficient TODO: is this a realistic value?
         alpha_c = 0.5 # cathode charge transfer coefficient TODO: is this a realistic value?
         i_0_a = (2 * 10**(-7)) # anode exchange current density TODO: update to be f(T)?
         i_0_c = (2 * 10**(-3)) # cathode exchange current density TODO: update to be f(T)?
-        I_in = (self.P_in*1000)/(self.stack_input_voltage*self.n_cells) # TODO: best way to calculate?
+        I_in = (P_in*1000)/(self.stack_input_voltage*self.n_cells) # TODO: best way to calculate?
         i = I_in/self.cell_area 
 
         V_act_a = ((self.R*T_anode)/(alpha_a*self.F)) * np.arcsinh(i/(2*i_0_a)) # derived from Butler-Volmer eqs
         V_act_c = ((self.R*T_cathode)/(alpha_c*self.F)) * np.arcsinh(i/(2*i_0_c)) # derived from Butler-Volmer eqs
 
         # Ohmic overpotential:
-        lambda_nafion = (((-2.89556 + (0.016 * T_K)) + 1.625) / 0.1875) # pulled from https://www.sciencedirect.com/science/article/pii/S0360319917309278?via%3Dihub 
-        t_nafion = 0.03 # (cm) TODO: confirm actual thickness
-        sigma_nafion = (((0.005139 * lambda_nafion)-0.00326) * math.exp(1268*((1/303)-(1/T_K))))
+        # pulled from https://www.sciencedirect.com/science/article/pii/S0360319917309278?via%3Dihub 
+        lambda_nafion = (((-2.89556 + (0.016 * T_K)) + 1.625) / 0.1875) # TODO: pulled from empirical data, is there a better eq?
+        t_nafion = 0.03 # (cm) TODO: confirm actual thickness?
+        sigma_nafion = (((0.005139 * lambda_nafion)-0.00326) * math.exp(1268*((1/303)-(1/T_K)))) # TODO: confirm with Nel, is there a better eq?
         R_ohmic = (t_nafion/sigma_nafion) #TODO: update to include electron resistance?
 
         # Cell / Stack voltage:
-        V_cell = (E_rev + V_act_a + V_act_c + (i*R_ohmic))
-        V_stack = V_cell * self.n_cells
+        self.V_cell = (E_rev + V_act_a + V_act_c + (i*R_ohmic))
+        V_stack = self.V_cell * self.n_cells
 
         # Stack Current
-        I = ((self.P_in*1000)/V_stack)
+        I = ((P_in*1000)/V_stack)
 
         return I 
 
 
-    def getH2GenRate(self, kw, eff=0.7):
-        hhv = 39.4  # kWh/kg
-        energy_req = hhv / eff
-        mflow = kw / energy_req
+    # def getH2GenRate(self, kw, eff=0.7):
+    #     hhv = 39.4  # kWh/kg
+    #     energy_req = hhv / eff
+    #     mflow = kw / energy_req
 
-        return mflow
+    #     return mflow
 
-    def calcMFRfromCurrent(self, amps, num_cells=100):
+    def faradaic_efficiency(self, I):
+
+        # https://res.mdpi.com/d_attachment/energies/energies-13-04792/article_deploy/energies-13-04792-v2.pdf
+        
+        f_1 = 250 #(mA2/cm4)
+        f_2 = 0.996
+        i_cell = I * 1000
+
+        n_F = ((((i_cell/self.cell_area)**2) / (f_1 + ((i_cell/self.cell_area)**2))) * f_2)
+    
+        return n_F
+
+    def electrolysis_efficiency(self):
+        n_Th = self.E_th_0/self.V_cell # TODO: Is a valid efficiency loss? confirm should we include i*ASR in denominator?
+
+        return n_Th
+
+    def total_efficiency(self, I):
+        n_F = self.faradaic_efficiency(I)
+        n_Th = self.electrolysis_efficiency()
+
+        n_Tot = n_F * n_Th
+
+        return n_Tot
+
+    def calcMFRfromCurrent(self, amps, n_Tot):
         """
         calculate a mass flow rate (kg/h) from the current input
         amps - current [A]
         num_cells - number of cells [-]; for a 750-kW stack, we had 100 cells
         """
-        mfr = 3.63 * 10 ** (-5) * amps * num_cells
+        mfr = 3.63 * 10 ** (-5) * amps * self.n_cells * n_Tot
         return mfr
 
 
@@ -184,10 +209,13 @@ class SimpleElectrolyzerModel(om.ExplicitComponent):
 
             # create a current profile
             # this h2_current will be sent to the physical stack
-            h2_current = self.elec.stack_current_from_power(Pdc_plus, 70)
+            h2_current = self.elec.stack_current_from_power(Pdc_plus, 60)
+
+            # Calculate efficiencies:
+            n_Tot = self.elec.total_efficiency(h2_current)
 
             # calculate a mass flow rate
-            h2_prod_rate_from_current = self.elec.calcMFRfromCurrent(h2_current)
+            h2_prod_rate_from_current = self.elec.calcMFRfromCurrent(h2_current, n_Tot)
 
             outputs["h2_prod_rate"] = h2_prod_rate_from_current
 
@@ -201,20 +229,23 @@ class SimpleElectrolyzerModel(om.ExplicitComponent):
 
                 # create a current profile
                 # this h2_current will be sent to the physical stack
-                h2_current = self.elec.stack_current_from_power(Pdc_plus, 70)
+                h2_current = self.elec.stack_current_from_power(Pdc_plus, 60)
+
+                # Calculate efficiencies:
+                n_Tot = self.elec.total_efficiency(h2_current)
 
                 # calculate a mass flow rate
-                h2_prod_rate_from_current = self.elec.calcMFRfromCurrent(h2_current) * num_stacks_at_full
+                h2_prod_rate_from_current = self.elec.calcMFRfromCurrent(h2_current, n_Tot) * num_stacks_at_full
 
                 leftover_power = power % self.elec_stack_size
                 Pdc_minus, Pdc_plus = self.elec.convert_ac_to_dc(leftover_power)
 
                 # create a current profile
                 # this h2_current will be sent to the physical stack
-                h2_current = self.elec.stack_current_from_power(Pdc_plus, 70)
+                h2_current = self.elec.stack_current_from_power(Pdc_plus, 60)
 
                 # calculate a mass flow rate
-                h2_prod_rate_from_current += self.elec.calcMFRfromCurrent(h2_current)
+                h2_prod_rate_from_current += self.elec.calcMFRfromCurrent(h2_current, n_Tot)
 
                 outputs["h2_prod_rate"][idx] = h2_prod_rate_from_current
 
